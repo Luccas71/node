9a9f00351fccd8cc5526a6ed5eabf79656822b13
Lucas Volpato <lmaciel916@gmail.com>
2024-03-22 18:44:29 +0000

    created server

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
 
 // criando rota padrão ou raiz
 app.get('/', (req, res) =>{
     res.send("Curso de Node JS!")
 })
 
-// observar a porta 3000
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
-})
+export default app
